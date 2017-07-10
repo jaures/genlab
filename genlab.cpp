f68dfe3dcc235d6f3e9a55051b120e01bd5aac02
Jaures <jauresade@gmail.com>
2017-07-10 02:39:09 -0700

    Working on Test Generation

--- genlab.cpp
+++ genlab.cpp
@@ -450,7 +450,7 @@ void arg_test()
 	std::cout << "Testing now...\n\n";
 
 
-    std::vector<std::vector<std::string> > tests = _parse_testFile();
+    std::vector<std::vector<std::string> > testFile = _parse_testFile();
 
 }
 
